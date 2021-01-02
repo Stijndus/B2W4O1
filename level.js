c517d0390107ec73b7ddd2a4bbbb1d56cb811e60
Stijn <stijn@ardusit.nl>
2021-01-03 00:42:09 +0100

    Commit #6
    Added a few levels and some quality of life changes

--- level.js
+++ level.js
@@ -1,4 +1,14 @@
-var weapon = false;
+var inventory = [{
+	weapon: false,
+},{
+	food: false,
+},
+{
+	meat:false,
+}];
+
+var greetings = new Audio('audio/greeting.mp3');
+var death = new Audio('audio/death.mp3')
 
 var levels = [{
 		'id':0,
@@ -13,7 +23,9 @@ var levels = [{
 					let random = Math.floor(Math.random() * 2);
 					if(random === 1){
 						actions.innerHTML =('<br>You have found some food');
+						inventory[1].food = true;
 						enablebtn('nextLevel', true);
+						enablebtn('findFood', false);
 					} else {
 						actions.innerHTML +=('<br>You did\'nt find some food \nTry again!');
 					}
@@ -28,7 +40,7 @@ var levels = [{
 					if(random === 1){
 						enablebtn('nextLevel', true);
 						enablebtn('findWeapon', false);
-						weapon = true;
+						inventory[0].weapon = true;
 						actions.innerHTML ='<br>You\'ve found a weapon';
 					} else {
 						actions.innerHTML += '<br>You haven\'t found a weapon';
@@ -39,7 +51,7 @@ var levels = [{
 				'id': 'nextLevel',
 				'text': 'Go outside',
 				'function':function() {
-					game(1)
+					game(1);
 				},
 				'enabled': false,
 			}
@@ -55,13 +67,14 @@ var levels = [{
 				'text': 'Fight',
 				'enabled': true,
 				'function': function () {
-					if(weapon === true){
-						alert('You won the fight');
+					if(inventory[0].weapon === true){
+						actions.innerHTML +='You won the fight';
 						enablebtn('nextLevel', true);
-						enablebtn('fight', false)
-						;
+						enablebtn('fight', false);
 					} else {
-						alert('You lost the fight');
+						game(10);
+						death.play();
+						actions.innerHTML += 'The cockroach overpowered you';
 					}
 				},
 			},
@@ -73,50 +86,120 @@ var levels = [{
 					let random = Math.floor(Math.random() * 2);
 					if(random === 1){
 						alert('You succesfully ran away from this confrontation');
-						levels[2].buttons[0].text = '';
+						game(3);
 					} else {
-						alert('The cockroach spotted you and mauled you to death');
+						game(10);
+						death.play();
+						actions.innerHTML += 'The cockroach spotted you and mauled you to death';
 					}
 				},
 			}, {
 				'id': 'nextLevel',
 				'text': 'Next Level',
 				'function': function() {
-					game(2)
+					game(2);
 				}
 			}
 		]
 	},
 	{
 		'id':2,
-		'title': '',
-		'description': '',
-		'background': "url('g')",
+		'title': 'Level 3',
+		'description': 'After you killed the cockroach you decide to loot its body',
+		'background': "url('img/cockroach.jpg')",
 		'buttons': [{
-				'id': '',
-				'text': '',
+				'id': 'loot',
+				'text': 'Loot body',
 				'enabled': true,
 				'function': function () {
-					
-				}
-			},
-			{
-				'id': '',
-				'text': '',
-				'enabled': true,
-				'function': function () {
-					let random = Math.floor(Math.random() * 2);
-					
+					actions.innerHTML +='You looted the body <br> You gained a piece of meat';
+					inventory[2].meat = true;
+					enablebtn('loot', false);
 				}
 			},
 			{
 				'id': 'nextLevel',
 				'text': 'Next Level',
+				'enabled': true,
 				'function':function() {
-					game()
+					game(3);
 				},
-				'enabled': false,
 			}
+		]
+	},
+	{
+		'id':3,
+		'title': 'Level 4',
+		'description': 'You encountered a stranger <br>What will you do?',
+		'background': "url('img/stranger.jpg')",
+		'buttons': [{
+				'id': 'talk',
+				'text': 'Talk to stranger',
+				'enabled': true,
+				'function': function () {
+					actions.innerHTML += '';
+					game(4);
+					greetings.play();
+				}					
+			},
+			{
+				'id': 'fight',
+				'text': 'Fight stranger',
+				'enabled': true,
+				'function':function() {
+					game(10);
+					death.play();
+					actions.innerHTML += 'You brought a knife to a gun fight'
+				},
+			}
+		]
+	},
+	{
+		'id':4,
+		'title': 'Level 5',
+		'description': 'Hello there<br>I haven\'t had much luck finding food the last few days<br>I have something for you in exchange for a piece of meat',
+		'background': "url('img/stranger.jpg')",
+		'buttons': [{
+				'id': 'giveMeat',
+				'text': 'Give a piece of meat',
+				'enabled': true,
+				'function': function () {
+					actions.innerHTML += 'You gave him your piece of meat';
+					enablebtn('quest', false);
+				}					
+			},
+			{
+				'id': 'quest',
+				'text': 'Get quest',
+				'enabled': false,
+				'function':function() {
+					actions.innerHTML += 'Here you have the location of a mech suit<br>It will help you with fighting enemies and exploring';
+					enablebtn('nextLevel', true);
+				},
+			},
+			{
+				'id': 'nextLevel',
+				'text': 'Next Level',
+				'enabled': false,
+				'function':function() {
+					game(5);
+				},
+			}
+		]
+	},
+	{
+		'id':10,
+		'title': 'You died',
+		'description': '',
+		'background': "url('img/dead.png')",
+		'buttons': [{
+				'id': 'repawn',
+				'text': 'Respawn',
+				'enabled': true,
+				'function': function () {
+					location.reload();
+				}					
+			},
 		]
 	},
 ]
